1f2c54767cd3abe4f62d689c2be656fee7bd8257
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-09-14 23:07:16 +0200

    HTML , CSS
    - Navigation bar
    - Event
    - Animation

---
+++ blog-system/js/navBar.js
@@ -0,0 +1,25 @@
+const navBar = document.getElementById("navBar")
+const navBarIcon = document.getElementById("navIcon")
+const navCont = document.getElementById("navCont")
+
+let navOpen = false
+
+
+navBar.addEventListener('click', function(){
+    if(navOpen == false){
+        navOpen = true
+        console.log("MOUSe")
+        navBar.style.width = '20%';
+        navBarIcon.style.color = '#ffffff00';
+        navCont.style.display = "block";
+    } else {
+        navOpen = false
+        console.log("MOUSe")
+        navBar.style.width = '5%';
+        navBarIcon.style.color = '#fff';
+        navCont.style.display = "none";
+
+    }
+    
+
+}) 
