7f02014621c34ec4b95e8e64c3b91c5edbb2b02d
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-09-16 16:29:53 +0200

    Skills bar

---
+++ blog-system/js/langSkills.js
@@ -0,0 +1,36 @@
+
+  
+  const skillsMap = {
+    HTML: 100,
+    CSS: 88,
+    PHP: 80,
+    JS: 45,
+    PY: 35,
+    C: 10,
+    Node: 35,
+    LUA: 100
+  };
+
+  const languageBoxes = document.querySelectorAll('.langugage_box');
+  let langugageSelected = false
+  languageBoxes.forEach((box) => {
+    box.addEventListener('mouseover', (event) => {
+        langugageSelected = true
+      const languageName = event.target.innerText.trim();
+      const skill = skillsMap[languageName];
+        document.getElementById("skillsContainer").style.opacity = 0.7;
+        document.getElementById("skill_name").innerText = languageName;
+        document.getElementById("skillBar").style.width = `${skill}%`;
+    });
+
+    box.addEventListener('mouseout', () => {
+        langugageSelected = false
+        setTimeout(function () {
+            if (langugageSelected == false) {
+
+                document.getElementById("skillsContainer").style.opacity = 0;
+            }
+        }, 500); 
+    });
+  });
+
