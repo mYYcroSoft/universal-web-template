0fec2ad513f5ed7c6f3e102ae62c76efe7b3d152
mYcroSoft <j.sivco22@gmail.com>
2023-09-17 19:55:28 +0200

    POST

--- portfolio/js/reTypeText.js
+++ portfolio/js/reTypeText.js
@@ -1,40 +1,41 @@
-const texts = ['myycro.', 'web developer.', 'student.']; // Seznam textů pro karusel
-let textIndex = 0; // Index aktuálního textu
-let charIndex = 0; // Index aktuálního znaku
+// TENTO KOD NENÍ MŮJ :()
+const texts = ['myycro.', 'web developer.', 'student.']; 
+let textIndex = 0; 
+let charIndex = 0; 
 
 const textElement = document.getElementById('feature-text');
 
 function type() {
   if (charIndex < texts[textIndex].length) {
-    // Získání znaku a přidání k aktuálnímu textu
+    
     textElement.textContent += texts[textIndex].charAt(charIndex);
     charIndex++;
-    setTimeout(type, 100); // Čekání před dalším znakem
+    setTimeout(type, 100); 
   } else {
-    // Počkejte na chvíli, než přepíšete text
+
     setTimeout(erase, 1000);
   }
 }
 
 function erase() {
-  // Spustíme mazání textu
+ 
   setTimeout(eraseText, 500);
 }
 
 function eraseText() {
   if (charIndex > 0) {
-    // Mazání znaku
+ 
     textElement.textContent = texts[textIndex].substring(0, charIndex - 1);
     charIndex--;
-    setTimeout(eraseText, 50); // Čekání před dalším mazáním
+    setTimeout(eraseText, 50); 
   } else {
-    // Přepnutí na další text a resetování indexů
+
     textIndex = (textIndex + 1) % texts.length;
     charIndex = 0;
-    // Počkejte na chvíli, než začnete znovu psát
+   
     setTimeout(type, 500);
   }
 }
 
-// Spuštění psaní textu
+
 type();
