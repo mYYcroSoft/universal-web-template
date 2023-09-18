5a94edae653fbb286428b85cf64956964bc7ebda
mYcroSoft <j.sivco22@gmail.com>
2023-09-18 14:26:04 +0200

    Buttons styles

--- portfolio/js/navBar.js
+++ portfolio/js/navBar.js
@@ -8,13 +8,13 @@ let navOpen = false
 navBar.addEventListener('click', function(){
     if(navOpen == false){
         navOpen = true
-        console.log("MOUSe")
+      
         navBar.style.width = '70%';
         navBarIcon.style.color = '#ffffff00';
         navCont.style.display = "block";
     } else {
         navOpen = false
-        console.log("MOUSe")
+    
         navBar.style.width = '5%';
         navBarIcon.style.color = '#fff';
         navCont.style.display = "none";
