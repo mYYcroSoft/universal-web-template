c622411769c7e6755c0f84e4ed46e491ef25c9dd
mYcroSoft <j.sivco22@gmail.com>
2023-11-15 14:07:39 +0100

    user authentications

--- portfolio/php-server-test/index.js
+++ portfolio/php-server-test/index.js
@@ -5,17 +5,17 @@ loginBtn.addEventListener('click', function(){
 var username = document.getElementById('username').value
 var password = document.getElementById('password').value
 
-  
+const php_login_dir = './php/login.php'
     
 $.ajax({
-    url: './auth.php',  
+    url:  './php/login.php',  
     type: 'POST',              
     data: {action: 'clientAuth',  username: username, password:password},  
     dataType: 'json',        
     success: function(response) {
             console.log(response)
             if (response['status'] == 'login'){
-                document.cookie =  {token: response['token']} 
+                addTokenToCookies(response['token'])
                 console.log(document.cookie);
             }
             /*writeTokenToCookie(response)*/
@@ -31,13 +31,14 @@ $.ajax({
 
 removeSes = document.getElementById('removeSes')
 removeSes.addEventListener('click', function(){
+
     $.ajax({
-        url: './auth.php',  
+        url: './php/login.php',  
         type: 'POST',              
         data: {action: 'removeSes'},  
         dataType: 'json',        
         success: function($reponse) {
-           
+           console.log($reponse);
                 
         }, 
         error: function(xhr, status, error) {
@@ -48,5 +49,20 @@ removeSes.addEventListener('click', function(){
 
 
 cookie.addEventListener('click', function(){
-    console.log(document.cookie);
+    console.log(getTokenFromCookies());
 })
+
+function addTokenToCookies(token) {
+    document.cookie = "userToken=" + token + "; path=/";
+}
+
+function getTokenFromCookies() {
+    const cookies = document.cookie.split(';');
+    for (const cookie of cookies) {
+        const [name, value] = cookie.trim().split('=');
+        if (name === 'userToken') {
+            return value;
+        }
+    }
+    return null;
+}
